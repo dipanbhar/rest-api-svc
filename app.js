be53e33eddd9f6160c025190ed9d9582a2241ebf
dipanbhar <dipanbhar@gmail.com>
2024-11-08 11:32:59 +0530

    Update app.js for swagger

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 import express  from 'express';
 import todoAPIs from "./src/routes/todo/TodoRoutes.js"
 import bodyParser from 'body-parser';
+import swaggerUI from 'swagger-ui-express';
+import YAML from 'yamljs';
 import cors from 'cors'
+
+const swaggerDocument = YAML.load('./swagger.yaml');
 const app = express();
 const port = process.env.port??3000;
 
 app.use(cors());
-
 app.use(bodyParser.json());
-
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 app.use("/",todoAPIs);
 
 app.listen(port, () => {
